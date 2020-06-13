d278d3fba4102867c0f5ad4409ece4debba211e8
Aveek Saha <aveek.s98@gmail.com>
2020-06-13 11:17:28 +0530

    Experimenting with rendering emoji

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,6 @@
 <script>
 import axios from "axios"
-import { fromHexcodeToCodepoint } from 'emojibase';
-import { fromCodepointToUnicode } from 'emojibase';
+import { fromHexcodeToCodepoint, fromCodepointToUnicode } from 'emojibase';
 
 let emojis = []
 
@@ -10,33 +9,38 @@ axios.get('https://api.github.com/emojis')
     // handle success
 	console.log(response);
 	emojis = Object.entries(response.data);
-	// emojis.forEach(em => {
-	// 	var code = fromHexcodeToCodepoint(em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].toUpperCase())
-	// 	// var code = em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].split("-").map(num => parseInt(num, 16))
-	// 	// try {
-	// 	// 	code = String.fromCodePoint(code);
-	// 	// }
-	// 	// catch(err) {
-	// 	// 	code = "<img src=\"" + em[1] + "\">"
-	// 	// 	console.log(err);	
-	// 	// }
-	// 	// em[1] = code == "NaN" ? "<img src=\"" + em[1] + "\">" :fromCodepointToUnicode(code)
-	// 	em[1] = "<img src=\"" + em[1] + "\">"
+	emojis.forEach(em => {
+		var code = fromHexcodeToCodepoint(em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].toUpperCase())
+		// var code = em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].split("-").map(num => parseInt(num, 16))
+		// try {
+		// 	code = String.fromCodePoint(code);
+		// }
+		// catch(err) {
+		// 	code = "<img src=\"" + em[1] + "\">"
+		// 	console.log(err);	
+		// }
+		em[1] = code == "NaN" ? "<img src=\"" + em[1] + "\">" : fromCodepointToUnicode(code)
+		// console.log(em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].toUpperCase().split("-"));
+		
+		// em[1] = code
+		// em[1] = "<img src=\"" + em[1] + "\">"
 
-	// });
+	});
 
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   })
+
+  
 </script>
 
 
 {#each emojis as emoji, index (emoji)}
-	<!-- <span style='font-size:30px;'> -->
+	<span style='font-size:30px;'>
 	<!-- {emoji[0]}:  -->
-	<!-- {@html emoji[1]} -->
-	<!-- </span> -->
-	<img src={emoji[1]} alt={emoji[0]}>
+	{ emoji[1]}
+	</span>
+	<!-- <img src={emoji[1]} alt={emoji[0]}> -->
 {/each}
