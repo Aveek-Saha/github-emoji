73511dc67d0b0af343efa9119be3110c39f67300
Aveek Saha <aveek.s98@gmail.com>
2020-06-15 00:04:44 +0530

    Display all emoji

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -15,22 +15,31 @@ axios.get('https://api.github.com/emojis')
 	axios.get('https://emoji-api.com/emojis?access_key=' + config.emoji_api)
 	.then((res) => {
 		// console.log(res.data);	
-		emojis = res.data
+		// emojis = res.data
 		arr.forEach(em => {
 			var hex = em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].toUpperCase()
 			var code = fromHexcodeToCodepoint(hex)
 			// var code = em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].split("-").map(num => parseInt(num, 16)
 			var dis = code == "NaN" ? "<img src=\"" + em[1] + "\">" : code.map(num => "&#" + num + ";").join("&zwj;")
-			// emojis.push({
-			// 	shortcode: em[0],
-			// 	display: dis
-			// })
-			console.log(emojis.find(x => x.codePoint === hex.split("-").join(" ")));
-			// console.log(hex.split("-").join(" "));
-
 			
-			// emojis.find(x => x.codePoint === code)
+			var data = res.data.find(x => x.codePoint === hex.split("-").join(" "));
+			// console.log(data);
+			if (data) {
+				data.image = em[1]
+				data.shortcode = em[0]
+			}
+			else{
+				data = {
+					display: code == "NaN" ? "<img src=\"" + em[1] + "\">" : code.map(num => "&#" + num + ";").join("&zwj;"),
+					image: em[1],
+					shortcode: em[0]
+				}
+			}
+			
+			emojis.push(data)
+			// console.log(hex.split("-").join(" "));
 		});
+		emojis = emojis
 	})
 
 	
@@ -45,11 +54,15 @@ axios.get('https://api.github.com/emojis')
 </script>
 
 
-{#each emojis as emoji, index (emoji)}
-	{#if ifEmoji(emoji.character)}
+{#each emojis as emoji}
+	{#if emoji.character && ifEmoji(emoji.character)}
 		<span>
-			{@html emoji.character}
+			{emoji.character}
+		</span>
+	{:else if emoji.display }
+		<!-- <img src={emoji.image} alt={emoji.shortcode}> -->
+		<span>
+			{@html emoji.display}
 		</span>
 	{/if}
-	<!-- <img src={emoji.display} alt={emoji.shortcode}> -->
 {/each}
