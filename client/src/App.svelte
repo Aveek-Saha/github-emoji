bd1a6e848b195df0ca812e07529ce5baaa134b9f
Aveek Saha <aveek.s98@gmail.com>
2022-03-03 20:45:27 +0530

    Fetch and store github emoji data

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,68 +1,18 @@
 <script>
-import axios from "axios"
-import { fromHexcodeToCodepoint, fromCodepointToUnicode } from 'emojibase';
-import ifEmoji from 'if-emoji'
 
-
-import * as config from "../config.json"
-
-let emojis = []
-
-axios.get('https://api.github.com/emojis')
-  .then(function (response) {
-    // handle success
-	var arr = Object.entries(response.data);
-	axios.get('https://emoji-api.com/emojis?access_key=' + config.emoji_api)
-	.then((res) => {
-		// console.log(res.data);	
-		// emojis = res.data
-		arr.forEach(em => {
-			var hex = em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].toUpperCase()
-			var code = fromHexcodeToCodepoint(hex)
-			// var code = em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].split("-").map(num => parseInt(num, 16)
-			var dis = code == "NaN" ? "<img src=\"" + em[1] + "\">" : code.map(num => "&#" + num + ";").join("&zwj;")
-			
-			var data = res.data.find(x => x.codePoint === hex.split("-").join(" "));
-			// console.log(data);
-			if (data) {
-				data.image = em[1]
-				data.shortcode = em[0]
-			}
-			else{
-				data = {
-					display: code == "NaN" ? "<img src=\"" + em[1] + "\">" : code.map(num => "&#" + num + ";").join("&zwj;"),
-					image: em[1],
-					shortcode: em[0]
-				}
-			}
-			
-			emojis.push(data)
-			// console.log(hex.split("-").join(" "));
-		});
-		emojis = emojis
-	})
-
-	
-
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
 
   
 </script>
 
 
-{#each emojis as emoji}
+<!-- {#each emojis as emoji}
 	{#if emoji.character && ifEmoji(emoji.character)}
 		<span>
 			{emoji.character}
 		</span>
 	{:else if emoji.display }
-		<!-- <img src={emoji.image} alt={emoji.shortcode}> -->
 		<span>
 			{@html emoji.display}
 		</span>
 	{/if}
-{/each}
+{/each} -->
---
+++ index.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+const path = require('path');
+const axios = require("axios");
+
+// import { fromHexcodeToCodepoint, fromCodepointToUnicode } from "emojibase";
+// import ifEmoji from "if-emoji";
+
+// import * as config from "../config.json";
+
+async function main() {
+    let emojis = [];
+    const githubEmojis = await axios.get("https://api.github.com/emojis");
+    const file_path = path.join("data", "github_emojis.json")
+
+    try {
+        fs.writeFileSync(file_path, JSON.stringify(githubEmojis.data));
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+main();
+
+// axios
+//     .get("https://api.github.com/emojis")
+//     .then(function (response) {
+//         // handle success
+//         var arr = Object.entries(response.data);
+//         axios
+//             .get("https://emoji-api.com/emojis?access_key=" + config.emoji_api)
+//             .then((res) => {
+//                 // console.log(res.data);
+//                 // emojis = res.data
+//                 arr.forEach((em) => {
+//                     var hex = em[1]
+//                         .split("?")[0]
+//                         .split("/")
+//                         .splice(-1)[0]
+//                         .split(".")[0]
+//                         .toUpperCase();
+//                     var code = fromHexcodeToCodepoint(hex);
+//                     // var code = em[1].split("?")[0].split("/").splice(-1)[0].split(".")[0].split("-").map(num => parseInt(num, 16)
+//                     var dis =
+//                         code == "NaN"
+//                             ? '<img src="' + em[1] + '">'
+//                             : code.map((num) => "&#" + num + ";").join("&zwj;");
+
+//                     var data = res.data.find(
+//                         (x) => x.codePoint === hex.split("-").join(" ")
+//                     );
+//                     // console.log(data);
+//                     if (data) {
+//                         data.image = em[1];
+//                         data.shortcode = em[0];
+//                     } else {
+//                         data = {
+//                             display:
+//                                 code == "NaN"
+//                                     ? '<img src="' + em[1] + '">'
+//                                     : code
+//                                           .map((num) => "&#" + num + ";")
+//                                           .join("&zwj;"),
+//                             image: em[1],
+//                             shortcode: em[0],
+//                         };
+//                     }
+
+//                     emojis.push(data);
+//                     // console.log(hex.split("-").join(" "));
+//                 });
+//                 emojis = emojis;
+//             });
+//     })
+//     .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//     });
