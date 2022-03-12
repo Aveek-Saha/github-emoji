b38a65868c64c5c66a781b84c3071ed737c687d8
Aveek Saha <aveek.s98@gmail.com>
2022-03-12 10:21:52 +0530

    Switch to array

--- index.js
+++ index.js
@@ -64,28 +64,30 @@ async function main() {
     const emojis_details = JSON.parse(fs.readFileSync(emojis_details_file_path, 'utf-8'))
     const github_emojis = JSON.parse(fs.readFileSync(file_path, 'utf-8'))
 
-    var shortcodes = {}
+    var shortcodes = []
 
     emojis_details.forEach(emoji => {
         if(emoji['hexcode'] in emoji_shortcodes){
             shortcode_name = emoji_shortcodes[emoji['hexcode']]
-            if(Array.isArray(shortcode_name)){
-                shortcode_name.forEach(shortcode => {
-                    shortcodes[shortcode] = emoji
-                });
-            }
-            else
-                shortcodes[shortcode_name] = emoji
+            // if(Array.isArray(shortcode_name)){
+            //     shortcode_name.forEach(shortcode => {
+            //         shortcodes[shortcode] = emoji
+            //     });
+            // }
+            // else
+            //     shortcodes[shortcode_name] = emoji
+            emoji["shortcode"] = shortcode_name
+            shortcodes.push(emoji)
         }
     });
-    var github_exclusive = {}
-    Object.entries(github_emojis).forEach(emoji => {
-        if(!(emoji[0] in shortcodes)){
-            github_exclusive[emoji[0]] = emoji[1]
-        }
-    });
+    // var github_exclusive = {}
+    // Object.entries(github_emojis).forEach(emoji => {
+    //     if(!(emoji[0] in shortcodes)){
+    //         github_exclusive[emoji[0]] = emoji[1]
+    //     }
+    // });
 
-    shortcodes["github_exclusive"] = github_exclusive
+    // shortcodes["github_exclusive"] = github_exclusive
 
 
     const emoji_cons_file_path = path.join("data", "emoji_cons.json");
