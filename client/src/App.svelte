51c2933438d3a364f21aaea4e8fda99eed1105a9
Aveek Saha <aveek.s98@gmail.com>
2022-03-27 12:04:22 +0530

    Redo svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,18 +1,30 @@
 <script>
-
-
-  
+	export let name;
 </script>
 
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
 
-<!-- {#each emojis as emoji}
-	{#if emoji.character && ifEmoji(emoji.character)}
-		<span>
-			{emoji.character}
-		</span>
-	{:else if emoji.display }
-		<span>
-			{@html emoji.display}
-		</span>
-	{/if}
-{/each} -->
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
