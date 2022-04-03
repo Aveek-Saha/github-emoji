cb42230f0fb96d0c5cfeed82da158e4a752ebfe0
Aveek Saha <aveek.s98@gmail.com>
2022-04-03 13:03:38 +0530

    Load emoji from json

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,10 @@
 <script>
-	export let name;
+	export let emojis;
+	let common = emojis['common']
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#each common as emoji}
+		<span>{emoji.emoji}</span>
+	{/each}
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,10 @@
 import App from './App.svelte';
+import * as emojiData from '../../data/github_emoji_shortcodes.json'; 
 
 const app = new App({
 	target: document.body,
 	props: {
-		name: 'world'
+		emojis: emojiData
 	}
 });
 
