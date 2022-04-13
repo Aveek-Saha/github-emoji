d9628ec86c2e4e35020b33a52f4c93aae5e30c78
Aveek Saha <aveek.s98@gmail.com>
2022-04-13 22:15:38 +0530

    Add toast

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,22 @@
 <script>
+	import { SvelteToast, toast } from "@zerodevx/svelte-toast";
+	const options = {};
     export let emojis;
     let common = emojis["common"];
 	let search = "";
 
     let colors = [ "#fbcfb7", "#7f39fb", "#fb584a", "#ffdb3a", "#ef5350", "#ff7281", "#fbb64b", "#ff5864", "#80deea", "#f94f28", "#8cd842", "#00a9f0", "#cedae6", "#64b5f6", "#ffb74d", "#ff9d44", "#e7f4ff", "#c7cb12", "#ffc400", "#d9e3e8", "#40c4ff", "#cb63e6", "#83beec", "#23b4d2", "#00e676", "#e6ebef", "#fdd0ae", "#4dc6dc", "#a78674", "#39c2f1", "#56d1d8", "#536dfe", "#8bdfe7", "#c5e763", "#f74d5f", "#ffce49", "#ef584a", "#00b1fb", "#77e856", "#ffe55f", "#ff6723", "#86B837"];
+
+	function createToast() {
+        toast.pop();
+        toast.push("Emoji shortname copied!", {
+            theme: {
+                "--toastBackground": "#48BB78",
+                "--toastProgressBackground": "#2F855A",
+            },
+            duration: 3000,
+        });
+    }
 
 	$: list = common
         ? common.filter(
@@ -44,7 +57,7 @@
         <div class="row row-cols-lg-6 row-cols-sm-2 row-cols-md-4 row-cols-xs-1">
             {#each list as emoji}
                 <div class="col mb-3 text-center">
-                    <div class="card h-100">
+                    <div class="card h-100" on:click={() => createToast()}>
                         <div class="card-header" style="background-color: {colors[Math.floor(Math.random()*colors.length)]};">
                             <p class="emoji">{emoji.emoji}</p>
                         </div>
@@ -57,3 +70,4 @@
         </div>
     </div>
 </main>
+<SvelteToast {options} />
