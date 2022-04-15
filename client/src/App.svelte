10fea82ce918d9ffb639a25d302089214839d58d
Aveek Saha <aveek.s98@gmail.com>
2022-04-15 22:26:14 +0530

    Organize colors

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,10 +3,10 @@
 	const options = {};
 	new ClipboardJS(".card");
     export let emojis;
+    export let color_codes;
     let common = emojis["common"];
+    let colors = color_codes["hex_codes"];
 	let search = "";
-
-    let colors = [ "#fbcfb7", "#7f39fb", "#fb584a", "#ffdb3a", "#ef5350", "#ff7281", "#fbb64b", "#ff5864", "#80deea", "#f94f28", "#8cd842", "#00a9f0", "#cedae6", "#64b5f6", "#ffb74d", "#ff9d44", "#e7f4ff", "#c7cb12", "#ffc400", "#d9e3e8", "#40c4ff", "#cb63e6", "#83beec", "#23b4d2", "#00e676", "#e6ebef", "#fdd0ae", "#4dc6dc", "#a78674", "#39c2f1", "#56d1d8", "#536dfe", "#8bdfe7", "#c5e763", "#f74d5f", "#ffce49", "#ef584a", "#00b1fb", "#77e856", "#ffe55f", "#ff6723", "#86B837"];
 
 	function createToast() {
         toast.pop();
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,12 @@
 import App from './App.svelte';
 import * as emojiData from '../../data/github_emoji_shortcodes.json'; 
+import * as colorData from '../../data/colors.json'; 
 
 const app = new App({
 	target: document.body,
 	props: {
-		emojis: emojiData
+		emojis: emojiData,
+		color_codes: colorData
 	}
 });
 
